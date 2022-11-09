2918ae61043e08e74fe38b7e2c311d3a9495ebe6
IvanPanin675 <107686584+IvanPanin675@users.noreply.github.com>
2022-11-09 11:42:01 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+const galleryEl = document.querySelector('.gallery');
+const markupGallery = galleryItems.map(({preview, description}) => 
+ `   <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="large-image.jpg"
+      alt="${description}"
+    />
+  </a>
+</div>`
+)
+.join("");
+galleryEl.insertAdjacentHTML("afterbegin", markupGallery);
+galleryEl.addEventListener("click", (event) => {
+    event.preventDefault();
+  });
 console.log(galleryItems);
