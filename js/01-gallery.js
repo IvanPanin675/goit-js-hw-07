f1af52b8d4438dab49cb6cf489147d9c1684a06a
IvanPanin675 <107686584+IvanPanin675@users.noreply.github.com>
2022-11-11 12:15:11 +0200

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,23 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-
+const body = document.body;
 const galleryEl = document.querySelector('.gallery');
-const markupGallery = galleryItems.map(({preview, description}) => 
- `   <div class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="large-image.jpg"
-      alt="${description}"
-    />
-  </a>
-</div>`
+const markupGallery = galleryItems.map(({preview, original, description}) => 
+ `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
 )
 .join("");
 galleryEl.insertAdjacentHTML("afterbegin", markupGallery);
+
+const createModalWindow = (imageAdress) => {
+  window.instance = basicLightbox.create(
+    `<img src="${imageAdress}">`
+    // ,
+    // {
+    //   onShow: () =>
+    //     window.addEventListener("keydown", closeModalWindowByEscPressing),
+    //   onClose: () => {
+    //     window.removeEventListener("keydown", closeModalWindowByEscPressing);
+    //     refs.body.classList.remove("disable-scroll");
+    //   },
+    // }
+  );
+  return instance;
+};
+
 galleryEl.addEventListener("click", (event) => {
     event.preventDefault();
+    if (!event.target.classList.contains("gallery__image")) {return;}    
+
+    const originalImageSource = event.target.dataset.source;
+    createModalWindow(originalImageSource).show();
+    body.classList.add(".disable-scroll");
+    console.log(event.target);
   });
+
+
+
 console.log(galleryItems);
