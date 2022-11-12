c3b8e132a156330ba3628bf5a5fd1c3fbc1c2bc1
IvanPanin675 <107686584+IvanPanin675@users.noreply.github.com>
2022-11-12 21:55:51 +0200

    danone

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,18 +4,28 @@ import { galleryItems } from './gallery-items.js';
 const body = document.body;
 const galleryEl = document.querySelector('.gallery');
 const markupGallery = galleryItems.map(({preview, original, description}) => 
- `<div class="gallery__item">
-    <a class="gallery__link" href="large-image.jpg">
+ `<li class="gallery__item">
+    <a class="gallery__link"  href="${original}">
       <img
+        loading="lazy" 
+        width="354" 
+        height="240"
         class="gallery__image"
         src="${preview}"
-        data-source="${original}"
         alt="${description}"
       />
     </a>
-  </div>`
+  </li>`
 )
 .join("");
 galleryEl.insertAdjacentHTML("afterbegin", markupGallery);
+
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  scrollZoom: false,
+});
+
 
 console.log(galleryItems);
